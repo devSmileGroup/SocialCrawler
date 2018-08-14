ac47f976462948dbfa0fe74147bfbe0e2d67ceb8
Vlad <vladmartishevskii@gmail.com>
2018-08-14 16:26:23 +0300

    Added Task, Post, TaskStatus, and Category classes

---
+++ src/main/java/com/crawler/crawler/models/Task.java
@@ -0,0 +1,39 @@
+package com.crawler.crawler.models;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.Data;
+
+@Data
+public class Task {
+
+	@Id
+	@JsonSerialize(using = ToStringSerializer.class)
+	private ObjectId id;
+
+	private List<String> hashtags;
+
+	private Category category;
+
+	private LocalDateTime from;
+
+	private LocalDateTime to;
+
+	private TaskStatus status;
+
+	@CreatedDate
+	@Field("created_date")
+	private LocalDateTime createdDate;
+	
+	@LastModifiedDate
+	@Field("modified_date")
+	private LocalDateTime modifiedDate;
+
+}
---
+++ src/main/java/com/crawler/crawler/models/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.crawler.crawler.models;
+
+public enum TaskStatus {
+	NEW, IN_PROCESS, PROCESSED, FAILED
+
+}
